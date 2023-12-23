a28f13b0f1729a321ce773cf61beb3bbcba446db
mljubic-biz <marko.ljubic@spottedzebra.co.uk>
2023-12-23 17:53:38 +0100

    Implement post router and post view

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,10 +1,45 @@
+import { clerkClient } from "@clerk/nextjs";
+import { type User } from "@clerk/nextjs/dist/types/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profilePicture: user.imageUrl,
+    firstName: user.firstName,
+    lastName: user.lastName
+  }
+}
+
 export const postRouter = createTRPCRouter({
   // Public procedure - no authentication required, everyone can access this.
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+    });
+
+    const users = await clerkClient.users.getUserList({
+      userId: posts.map(post => post.authorId),
+      limit: 100
+    });
+
+    const postsWithAuthor = posts.map(post => {
+      const author = users.find(user => user.id === post.authorId);
+      console.log(author);
+      if (!author) {
+        throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message: "Author not found"});
+      }
+
+      return {
+        ...post,
+        author: { ...filterUserForClient(author), username: author.username }
+      }
+    });
+
+    return postsWithAuthor;
   }),
 });
